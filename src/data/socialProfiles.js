5282b2762d893fbc5550fa40a53e5fe953ef9a2a
unknown <parnal.patil@gmail.com>
2020-09-14 17:28:53 +0530

    Portfolio Project using React

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,30 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id:1,
+        link: 'mailto:parnal.patil@gmail.com',
+        image: emailIcon
+    },
+    {
+        id:2,
+        link: 'https://github.com/ParnalPatil?tab=repositories',
+        image: githubIcon
+    },
+    {
+        id:3,
+        link: 'https://in.linkedin.com/in/parnal-ghundare-patil-48758b2b',
+        image: linkedinIcon
+    },
+    {
+        id:4,
+        link: 'https://twitter.com/parnal1996',
+        image: twitterIcon
+    },
+];
+
+
+export default SOCIAL_PROFILES;
